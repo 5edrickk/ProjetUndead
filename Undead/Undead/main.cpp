50cb4b7ec1fc653ddbc3e6819704a5222bf353b5
AceFinka <davegamersbk@gmail.com>
2025-04-23 10:48:31 -0400

    Création du projet vide

---
+++ Undead/Undead/main.cpp
@@ -0,0 +1,16 @@
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp> // Librairie pour faire jouer des sons en SFML
+#include <string>
+#include <iostream>
+
+using namespace sf;
+using namespace std;
+
+int main() {
+
+
+
+	system("pause");
+	return 0;
+
+}
