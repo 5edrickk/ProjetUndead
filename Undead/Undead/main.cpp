3e9d0db8df394340f16f050208f8c66c995da192
AceFinka <davegamersbk@gmail.com>
2025-04-23 11:47:39 -0400

    Test

--- Undead/Undead/main.cpp
+++ Undead/Undead/main.cpp
@@ -3,11 +3,16 @@
 #include <string>
 #include <iostream>
 
+#include "functions.h"
+
 using namespace sf;
 using namespace std;
 
 int main() {
+
+	fTest();
 	
+	cout << endl;
 	system("pause");
 	return 0;
 
